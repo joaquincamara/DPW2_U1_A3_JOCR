e1f9562a1fd575db88d782a776a9a9457dd60824
Joaquin Camara <jcamara@NSL-JCAMARAs-MacBook-Pro.local>
2020-03-23 17:35:56 -0600

    navbar with user verificarion

--- partials/header.php
+++ partials/header.php
@@ -1,20 +1,28 @@
+<?php 
+
+  require 'database.php'
+
+?>
+
 <header>
-<ul class="nav">
+  <ul class="nav">
+    <?php if(!empty($inicio)): ?>
       <li class="nav-item">
         <a href="./courses.php" class="nav-link active" href="#">Mis Cursos</a>
       </li>
       <li class="nav-item">
         <a href="./player.php" class="nav-link" href="#">Reproductor</a>
       </li>
+      <li class="nav-item">
+        <a href="logout.php" class="nav-link">Logout</a>
+      </li>
+    <?php endif; ?>
       <li  class="nav-item">
         <a href="login.php" class="nav-link">Login</a>
       </li>
       <li  class="nav-item">
         <a href="signup.php" class="nav-link">SignUp</a>
       </li>
-      <li class="nav-item">
-        <a href="logout.php" class="nav-link">Logout</a>
-      </li>
-    </ul>
+  </ul>
 </header>
 
